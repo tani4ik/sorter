a893a059464a291adf50c30809698a89dec8cd53
Tatsiana Sakalinskaya <tani4ik@gmail.com>
2018-09-21 21:16:48 +0300

    Implemented class Sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
+    this.compareFunction = function (a,b) {
+      if (a < b) {
+         return -1;
+      } else if (a === b) {
+         return 0;
+      } else {
+         return 1;
+      }
+   }
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    for (let i = 0; i < this.array.length - 1; i++) {
+      for (let j = i + 1; j < this.array.length; j++) {
+         if (indices.indexOf(i) >= 0 && indices.indexOf(j) >= 0 && this.compareFunction(this.array[i], this.array[j]) > 0) {
+            let tmp = this.array[j];
+            this.array[j] = this.array[i];
+            this.array[i] = tmp;
+         }
+      }
+    }
+    return this.array;
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
